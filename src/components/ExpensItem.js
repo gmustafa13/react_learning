340588cc04206825cf2f6dd0000fe67b23ed2c3f
gmustafa <gmustafa.mansoori@gmail.com>
2021-10-24 20:48:39 +0530

    first commit

---
+++ src/components/ExpensItem.js
@@ -0,0 +1,15 @@
+import './ExpensItem.css'
+import ExpenseDate  from './ExpenseDate';
+const Expensitem = (props) => {
+    
+    return (
+        <div className='expense-item'>
+            <ExpenseDate date={ props.date}/>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>
+    )
+}
+export default Expensitem;
